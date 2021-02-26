1bdcee5567eadfbd5637abb1e6ef45803cd33ccd
Joaquimlagos <joaquim.lagos2000@gmail.com>
2021-02-26 17:10:34 -0300

    2° aula nlw4 nodejs concluida

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database"
+import { router } from './routes';
 
 const app = express();
 
-
-
-app.get("/", (req , res) => {
-
-    return res.json({ message: "Hello word - NLW4"});
-});
-
-app.post("/" , (req , res) => {
-
-    return res.json({ message: "Os dados foram salvos com sucesso!"});
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3303, () => console.log("Server is running"));
